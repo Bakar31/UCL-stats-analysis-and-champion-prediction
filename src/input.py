d0640833b75f6ec6d77d415cd4a1ec2ac30aa13e
Bakar31 <abubakar1808031@gmail.com>
2021-07-28 23:15:52 +0600

    input

--- src/input.py
+++ src/input.py
@@ -1,6 +1,7 @@
 # import basic libraries
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # import the dataset
 df = pd.read_csv('data/ucl_stats.csv')
--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,2 +1,19 @@
 from input import *
 
+# dropping columns that are not important for the model.
+df.drop(['year', 'team'], axis = 1, inplace = True)
+
+# train and dev set
+train = df.loc[:550]
+dev = df.loc[551:]
+
+#print(train.champions.value_counts())
+#print(dev.champions.value_counts())
+
+x = train.drop('champions', axis = 1)
+y = train['champions']
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state = 31)
+
+
