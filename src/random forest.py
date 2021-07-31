33dd80e0eab57048d5420dc09300fe3d0d371b7d
Bakar31 <abubakar1808031@gmail.com>
2021-07-31 23:00:02 +0600

    random forest parameter tune

--- src/random forest.py
+++ src/random forest.py
@@ -1,14 +1,15 @@
 from pre_processing import *
 from sklearn.ensemble import RandomForestClassifier
 
-rand_clf = RandomForestClassifier(n_estimators=1000, random_state = 35)
-rand_clf.fit(x_sm_train, y_sm_train)
+# parameters were taken by randomizedcvsearch
+rand_clf = RandomForestClassifier(n_estimators=1000,
+                                 min_samples_split = 4,
+                                 min_samples_leaf = 1,
+                                 max_depth = None,
+                                 random_state = 35)
+rand_clf.fit(x_train, y_train)
 print(rand_clf.score(x_test, y_test))
 
 from sklearn.metrics import classification_report
 y_preds = rand_clf.predict(x_test)
 print(classification_report(y_test, y_preds))
-
-dev_preds = rand_clf.predict(devx)
-print(rand_clf.score(devx, devy))
-print(classification_report(devy, dev_preds))
