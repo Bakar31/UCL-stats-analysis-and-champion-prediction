bcc85906d54ba4c06cde5a79d4d5d3c42858b649
Bakar31 <abubakar1808031@gmail.com>
2021-07-31 19:38:43 +0600

    logistic regression

--- src/logistic regression.py
+++ src/logistic regression.py
@@ -1,6 +1,5 @@
 from pre_processing import *
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
 
 log_clf = LogisticRegression(max_iter = 100,
                                  C = 5.428675439323859,
@@ -13,4 +12,3 @@ print(log_clf.score(x_test, y_test))
 from sklearn.metrics import classification_report
 y_preds = log_clf.predict(x_test)
 print(classification_report(y_test, y_preds))
-
